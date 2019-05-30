99c19ea9f0ec069a6408da5a9abd4efdc211cee0
Iuri Sajaia <i.sajaia@owl.local>
2019-05-30 17:44:39 +0400

    add tasks test route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/tasks', function (){
+    $tasks = [
+        'Login Page For Admin',
+        'Forgot Password For Users',
+        'Admin Can Add New User'
+    ];
+
+    return response()->json(['tasks' => $tasks]);
+});

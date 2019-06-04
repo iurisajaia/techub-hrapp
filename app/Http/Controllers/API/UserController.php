0064ca0380e39dae02f5fedd69d7cb5872a8a53f
Iuri Sajaia <i.sajaia@owl.local>
2019-06-04 14:58:12 +0400

    login error handling

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,16 +16,26 @@
 
         /* login api */ 
 
-        public function login(){ 
-          
-            if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
-                $user = Auth::user(); 
-                $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-                return response()->json(['success' => $success, 'user' => $user], $this-> successStatus); 
+        public function login(Request $request){ 
+            
+            if(!$request->email || !$request->password){
+                return response()->json('all fields are required');
             }
-            else{ 
-                return response()->json(['error'=>'Incorrect Data'], 401); 
-            } 
+
+            $user = User::where('email' , $request->email)->first();
+            if(!$user){
+                return response()->json('email not found');
+            }else{
+
+                if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+                    $user = Auth::user(); 
+                    $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+                    return response()->json(['success' => $success, 'user' => $user], $this-> successStatus); 
+                }
+                else{ 
+                    return response()->json('Incorrect Password', 401); 
+                }
+            }
         }
         
 

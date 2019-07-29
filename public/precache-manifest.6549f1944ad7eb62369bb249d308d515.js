0f831425d91960b71f50950a3babb545a2ec84e7
iuri sajaia <sajaiaiuri@gmail.com>
2019-07-29 12:13:39 +0400

    add builded react

---
+++ public/precache-manifest.6549f1944ad7eb62369bb249d308d515.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ed6c1e414ae1de1d53c5c9a347fa90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c198dd076c18d9c4cc94",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "1dbfdfaf41c6666832c0",
+    "url": "/static/css/main.bca66449.chunk.css"
+  },
+  {
+    "revision": "c198dd076c18d9c4cc94",
+    "url": "/static/js/2.9f679be1.chunk.js"
+  },
+  {
+    "revision": "1dbfdfaf41c6666832c0",
+    "url": "/static/js/main.5f7bb816.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59ffaba8fd4d67e642d90fb4b555c4aa",
+    "url": "/static/media/icons8-filter.59ffaba8.png"
+  },
+  {
+    "revision": "7ee3396588cfb9ed5fe9bb5b18d4e110",
+    "url": "/static/media/logo.7ee33965.png"
+  },
+  {
+    "revision": "a7e42f974005dc335d40ccd194261c2a",
+    "url": "/static/media/menu.a7e42f97.png"
+  },
+  {
+    "revision": "c884c4e26e30802794446642818c66fb",
+    "url": "/static/media/technologies.c884c4e2.png"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/static/media/trash-solid.a3e4c2f7.svg"
+  },
+  {
+    "revision": "fff37145498a8448ce274f0633380a15",
+    "url": "/static/media/userEdit.fff37145.png"
+  }
+]);
